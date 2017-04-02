b67e74a161efda7899a2c9d92ef049f8219bcf37
Mohamad Qaddura <msqaddura@outlook.com>
2017-04-02 23:52:58 +0300

    Merge branch 'feature/structure' into develop

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,7 @@
-
-// import { sayHello } from "./greet";
-
-// function showHello(divName: string, name: string) {
-//     const elt = document.getElementById(divName);
-//     elt.innerText = sayHello(name);
-//     console.info(PIXI);
-// }
-
-// showHello("greeting", "TypeScript");
 import { MainSceneBlueprint } from "./Game/MainSceneBlueprint";
 import { Application } from './Foundation/GameObjects/Application';
 
-
-
-function loadProgressHandler() {
-  console.log("loading"); 
-}
-
 function setup() {
-  // console.log("setup");
-  // const cb = new ComponentBuilder(MainSceneStructure);
-  // console.info(cb);
-  // document.body.appendChild(cb.root.canvas);
-  // console.info(AutoLayoutAdapter.getInstance());
   let app = new Application(null,{
     vfl:["HV:|[MainScene(100%)]|"],
     name:"Application"
