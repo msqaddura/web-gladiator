dd8d426f25a8d2cd602ad2f415c658bb186b041d
Mohamad Qaddura <msqaddura@outlook.com>
2017-04-02 18:15:21 +0300

    bye

--- src/Component/Chained/Texture.ts
+++
@@ -1,9 +0,0 @@
-import { Leaf } from '../Base/Leaf'
-export class Texture extends Leaf{
-    $loot;
-    constructor(owner, params){
-        super(owner, params);
-        this.$loot= PIXI.utils.TextureCache["Resources/squareC.png"];
-    }
-    
-}
