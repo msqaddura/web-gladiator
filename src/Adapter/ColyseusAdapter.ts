c2760a57bc61d4e432a3b4a36ed768f160c322a2
Mohamad Qaddura <msqaddura@outlook.com>
2017-09-27 23:48:48 +0300

    fix colyseus

--- src/Adapter/ColyseusAdapter.ts
+++ src/Adapter/ColyseusAdapter.ts
@@ -11,7 +11,7 @@ export class ColyseusAdapter {
         this._stream = new Rx.Subject();
         this._url = url;
         console.info(Colyseus);
-        this._client =  new Colyseus.Client(`${window.location.origin.replace("http","ws")}`);
+        this._client =  new Colyseus.Client(this._url);
         this._roomName = roomName;
     }
 
