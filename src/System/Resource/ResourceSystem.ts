10167e53de178925c42c5bf23f4a6aefe42f7a88
Mohamad Qaddura <msqaddura@outlook.com>
2019-05-08 01:18:56 +0300

    litle changed

--- src/System/Resource/ResourceSystem.ts
+++ src/System/Resource/ResourceSystem.ts
@@ -1,24 +1,26 @@
+import { Observable } from "rxjs";
+
 export class ResourceSystem {
-    static instance: ResourceSystem;
-    static getInstance() {
-        if (!this.instance) {
-            this.instance = new ResourceSystem();
-            // this.instance._adapter = new PixiResourceAdapter();
-            // ... any one time initialization goes here ...
-        }
-        return this.instance;
+  static instance: ResourceSystem;
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new ResourceSystem();
+      // this.instance._adapter = new PixiResourceAdapter();
+      // ... any one time initialization goes here ...
     }
-    _adapter;
-    constructor() {
-        //
-    }
-    use(adapter) {
-        this._adapter = adapter;
-    }
-    preloadManifest(manifest) {
-        return this._adapter.preloadManifest(manifest);
-    }
-    preload(file) {
-        return this._adapter.preload(file);
-    }
+    return this.instance;
+  }
+  _adapter;
+  constructor() {
+    //
+  }
+  use(adapter) {
+    this._adapter = adapter;
+  }
+  preloadManifest(manifest) {
+    return this._adapter.preloadManifest(manifest);
+  }
+  preload(file): Observable<number> {
+    return this._adapter.preload(file);
+  }
 }
