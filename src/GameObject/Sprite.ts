3f5485c79a33cf73eec72baafaa41e4f7f8f9333
Mohamad Qaddura <msqaddura@outlook.com>
2019-05-08 02:11:47 +0300

    fix lazy sprie

--- src/GameObject/Sprite.ts
+++ src/GameObject/Sprite.ts
@@ -4,12 +4,28 @@ import { GameObjectBuilder } from "../Foundation/Builder/GameObjectBuilder";
 import { View } from "../Foundation/Core/View";
 
 export class Sprite extends View {
+  constructor(owner, params) {
+    super(owner, params);
+    const texture = PIXI.Texture.EMPTY;
+    //PIXI.utils.TextureCache[params.fromImage];
 
-    constructor(owner, params) {
-        super(owner, params);
-        const texture = PIXI.utils.TextureCache[params.source];
-        this.view = new PIXI.Sprite(texture);
+    this.view = new PIXI.Sprite(texture);
+    this.view.twin = this;
+  }
+
+  preInitialize() {
+    super.preInitialize();
+    if (this.params.hasOwnProperty("fromImage")) {
+      this.view.texture = PIXI.Texture.from(this.params.fromImage);
     }
+  }
+
+  set fromImage(value) {
+    this.view.texture = PIXI.Texture.from(value);
+  }
+  get fromImage() {
+    return this.view.texture;
+  }
 }
 
 GameObjectBuilder.getInstance().registerGameObject("Sprite", Sprite);
